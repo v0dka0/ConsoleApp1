511f0a6def75bf91f5721276d1590dbd7a951b3c
208556 <208556@KB-118-C16>
2022-09-22 11:05:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ProgramMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class ProgramMain
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = rand.Next(1000);
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int minchet = ar[0];
+            int minnechet = ar[0];
+            for (int i = 0; i < ar.Length - 1; i++)
+            {
+                if (ar[i] % 2 == 0 && ar[i] < minchet && ar[i] != 0)
+                    minchet = ar[i];
+                if (ar[i] % 2 != 0 && ar[i] < minnechet && ar[i] != 0)
+                    minnechet = ar[i];
+            }
+            Console.WriteLine(minchet);
+            Console.WriteLine(minnechet);
+            Console.WriteLine();
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 > 0 && ar[i] > minnechet && ar[i] != 0)
+                        ar[i] = ar[i] - minnechet;
+            }
+            
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0 && ar[i] > minchet && ar[i] != 0)
+                        ar[i] = ar[i] - minchet;
+            }
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] a = new int[20];
+            int minchet = 0;
+            int minnechet = 0;
+            Random rand = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(0, 25);
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            minchet = a[0];
+            minnechet = a[0];
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] % 2 == 0 && a[i] < minchet && a[i] != 0)
+                    minchet = a[i];
+                if (a[i] % 2 != 0 && a[i] < minnechet && a[i] != 0)
+                    minnechet = a[i];
+            }
+            Console.WriteLine(minchet);
+            Console.WriteLine(minnechet);
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] % 2 != 0 && a[i] != 0)
+                    for (i = 0; i < a.Length; i++)
+                    {
+                        a[i] = a[i] - minnechet;
+                    }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] % 2 == 0 && a[i] != 0)
+                    for (i = 0; i < a.Length; i++)
+                    {
+                        a[i] = a[i] - minchet;
+                    }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
